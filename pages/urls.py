ada3e2ef611ea13823f04ebbb5fba2cb79a722ab
SYEONIH <syeonih@naver.com>
2025-01-14 20:44:02 +0900

    create main, about, category1, notice, login

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path('', views.mainpage),
  path('company/', views.company),
+
+ # 추가
+ path('', views.main, name='main'),
+ path('login/', views.login, name='login'),
+ path('about/', views.about, name='about'),
+ path('category1/', views.category1, name='category1'),
+ path('notice/',views.notice, name='notice' )
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,20 @@ def mainpage(request):
  return render(request, 'pages/mainpage.html')
 def company(request):
  return render(request, 'pages/company_info.html')
+
+
+# 추가 
+def main(request):
+    return render(request, 'pages/main.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def category1(request):
+    return render(request, 'pages/category1.html')
+
+def notice(request):
+    return render(request, 'pages/notice.html')
